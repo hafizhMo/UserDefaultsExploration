88d2d7705ee17614be13e27b2ab2e837c6d7788a
hafizhMo <hafizh.hizb@gmail.com>
2022-04-11 06:34:27 +0700

    plain method to use UserDefaults

--- UserDefaultsExploration/ViewController.swift
+++ UserDefaultsExploration/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchView: UISwitch!
+    let switchKey = "switchIsOn"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let isOn = UserDefaults.standard.bool(forKey: switchKey)
+        updateBackground(isOn: isOn)
     }
 
 
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        UserDefaults.standard.setValue(sender.isOn, forKey: switchKey)
+        updateBackground(isOn: sender.isOn)
+    }
+    
+    private func updateBackground(isOn: Bool){
+        view.backgroundColor = isOn ? .orange : .darkGray
+        switchView.isOn = isOn
+    }
 }
 
